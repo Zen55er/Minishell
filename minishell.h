38674024cb0f184d2644177b1c8fde6e086867a4
“gacorrei” <gguerrafranco@gmail.com>
2023-05-15 16:39:54 +0100

    Added libraries

--- minishell.h
+++ minishell.h
@@ -6,12 +6,27 @@
 /*   By: gacorrei <gacorrei@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/15 14:13:43 by gacorrei          #+#    #+#             */
-/*   Updated: 2023/05/15 15:45:48 by gacorrei         ###   ########.fr       */
+/*   Updated: 2023/05/15 16:39:35 by gacorrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
+
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <sys/stat.h>
+# include <sys/types.h>
+# include <dirent.h>
+# include <string.h>
+# include <sys/ioctl.h>
+# include <termios.h>
+# include <curses.h>
+# include <term.h>
 
 typedef struct s_minishell
 {
