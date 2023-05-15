d0883a9a5bda50d005ce3124dcc40047eb6f9eb2
“gacorrei” <gguerrafranco@gmail.com>
2023-05-15 15:47:34 +0100

    changed t_env

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: gacorrei <gacorrei@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/15 14:13:43 by gacorrei          #+#    #+#             */
-/*   Updated: 2023/05/15 14:24:45 by gacorrei         ###   ########.fr       */
+/*   Updated: 2023/05/15 15:45:48 by gacorrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 typedef struct s_minishell
 {
-	t_env	environment;
+	t_env	env;
 
 }			t_minishell;
 #endif
