36d02aa008b31d159fe3fd2ffa539a9143ba947e
MateusPMG <mpmgboss@gmail.com>
2023-06-27 15:25:31 +0100

    stuck

--- pipes.c
+++ pipes.c
@@ -6,21 +6,40 @@
 /*   By: mpatrao <mpatrao@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 11:51:33 by mpatrao           #+#    #+#             */
-/*   Updated: 2023/06/22 16:13:19 by mpatrao          ###   ########.fr       */
+/*   Updated: 2023/06/27 15:25:12 by mpatrao          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	check_fd_in()
-{
-	
-}
-
-void	child_exec_cmd(t_data *data, int pipefd[2], int in, t_cmd_st *node)
+int	check_fd_in(t_cmd_st *node)
 {
 	int	i;
 
+	i = 0;
+	if (node->redir_in != 0)
+		i = node->redir_in;
+	return (i);
+}
+
+int	check_fd_out(t_cmd_st *node)
+{
+	int	i;
+
+	i = 1;
+	if (node->redir_in != 0)
+		i = node->redir_out;
+	return (i);
+}
+
+void	child_exec_cmd(t_data *data, int pipefd[2], int in, int out)
+{
+	int	i;
+
+	/* default: if first node its stdinput
+	if its any in the middle its the previous pipe
+	IF THERE ARE ANY REDIRS THOSE ARE THE PRIORITY
+	(Still need to implement this logic) */
 	if (dup2(in, STDIN_FILENO) < 0)
 		error_handling();
 	close(pipefd[0]);
@@ -30,12 +49,11 @@ void	child_exec_cmd(t_data *data, int pipefd[2], int in, t_cmd_st *node)
 	if (data->cmd_st->prev)
 		close(in);
 	/* fazer em conjunto cm gabriel deixar para ultimo */
-	i = command_check(data->cmd_st->cmd);
-	command_call(data, 0);
-	
+	/* i = command_check(data->cmd_st->cmd);
+	command_call(data, 0); */
 }
 
-void	forking(t_data *data, int pipefd[2], int in, t_cmd_st *node)
+void	forking(t_data *data, int pipefd[2], int in, int out)
 {
 	static int	i;
 
@@ -43,28 +61,36 @@ void	forking(t_data *data, int pipefd[2], int in, t_cmd_st *node)
 	if (data->pid[i] < 0)
 		/* error handling */;
 	if (data->pid[i] == 0)
-		child_exec_cmd(data, pipefd, in, node);
+		child_exec_cmd(data, pipefd, in, out);
 	i++;
 	return (0);
 }
 
 int	pipeline(t_data *data)
 {
-	int	pipefd[2];
-	int	fd_in;
+	int			pipefd[2];
+	int			fd_in;
+	int			fd_out;
+	t_cmd_st	*tmp;
 
-	fd_in = STDIN_FILENO;
-	while (data->cmd_st)
+	tmp = data->cmd_st;
+	while (tmp)
 	{
-		if (data->cmd_st->next)
+		if (tmp->next)
 			pipe(pipefd);
-		fd_in = check_fd_in();
-		forking(data, pipefd, fd_in, data->cmd_st);
+		fd_in = check_fd_in(tmp);
+		fd_out = check_fd_out(tmp);
+		/*this forking func has to be 
+		conditional i think?
+		what happens when theres only one node?
+		pipefd[] will be full of 0s*/
+		/* how do we deal with a single node? ask gabriel*/
+		forking(data, pipefd, fd_in, fd_out);
 		close(pipefd[1]);
-		if (data->cmd_st->prev)
+		if (tmp->prev)
 			close(fd_in);
-		if (data->cmd_st->next)
-			data->cmd_st = data->cmd_st->next;
+		if (tmp->next)
+			tmp = tmp->next;
 		else
 			break ;
 	}
@@ -72,3 +98,4 @@ int	pipeline(t_data *data)
 	normal_command();
 	return (0);
 }
+
