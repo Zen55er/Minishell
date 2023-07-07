65ce1222f4b6678b2d2f6da03f55e8187c097d1c
“gacorrei” <gguerrafranco@gmail.com>
2023-07-07 13:32:05 +0100

    FIxed checking wrong index in cd arg check

--- directories.c
+++ directories.c
@@ -6,7 +6,7 @@
 /*   By: gacorrei <gacorrei@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/28 11:36:43 by gacorrei          #+#    #+#             */
-/*   Updated: 2023/07/07 13:26:39 by gacorrei         ###   ########.fr       */
+/*   Updated: 2023/07/07 13:30:03 by gacorrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,7 +75,7 @@ int	cmd_cd(t_data *data, char **tokens, int token)
 	char	*dir;
 
 	token++;
-	if ((tokens[token] && !tokens[token][1]) || (tokens[token]
+	if ((tokens[token] && !tokens[token][0]) || (tokens[token]
 			&& tokens[token + 1]
 			&& !delim_tok(tokens[token])
 			&& !delim_tok(tokens[token + 1])))
